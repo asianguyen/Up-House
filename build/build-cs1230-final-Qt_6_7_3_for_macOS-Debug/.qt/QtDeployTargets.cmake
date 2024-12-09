0dbd34494d2dbac835d16e09a9b251a2a7860e57
asianguyen <asia_nguyen@brown.edu>
2024-12-08 23:04:07 -0500

    house almost done

---
+++ build/build-cs1230-final-Qt_6_7_3_for_macOS-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,4 @@
+set(__QT_DEPLOY_TARGET_projects_realtime_FILE /Users/asianguyen/Desktop/CS1230/cs1230-final/build/build-cs1230-final-Qt_6_7_3_for_macOS-Debug/projects_realtime)
+set(__QT_DEPLOY_TARGET_projects_realtime_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_StaticGLEW_FILE /Users/asianguyen/Desktop/CS1230/cs1230-final/build/build-cs1230-final-Qt_6_7_3_for_macOS-Debug/libStaticGLEW.a)
+set(__QT_DEPLOY_TARGET_StaticGLEW_TYPE STATIC_LIBRARY)
