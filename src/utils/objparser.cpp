a781e90ee6e94ccea90bf6a63a51726714b2711d
asianguyen <113648504+asianguyen@users.noreply.github.com>
2024-12-11 15:53:25 -0500

    Update objparser.cpp

--- src/utils/objparser.cpp
+++ src/utils/objparser.cpp
@@ -4,8 +4,8 @@
 
 #include <iostream>
 #include <fstream>
+#include <map>
 #include <sstream>
-#include <cstring>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -19,6 +19,9 @@ struct Material {
     float d = 1.0f;        // Transparency (default to opaque)
     std::string map_Kd;    // Diffuse texture map
 };
+
+std::vector<Balloon> objparser::balloons;
+std::map<int, std::vector<size_t>> objparser::balloonVertexIndices;
 
 // Function to parse an .mtl file
 std::unordered_map<std::string, Material> parseMTL(const std::string& filepath) {
@@ -90,9 +93,13 @@ std::unordered_map<std::string, Material> parseMTL(const std::string& filepath) 
 
 bool objparser::loadOBJ(const char * path, std::vector<float> & out_vertices){
 
+    std::string mtlPath = path;
+    size_t dotPosition = mtlPath.find_last_of('.');
+    if (dotPosition != std::string::npos) {
+        mtlPath.replace(dotPosition, std::string::npos, ".mtl");
+    }
 
-    // std::unordered_map<std::string, Material> materials = parseMTL("/Users/sophialim/Desktop/CS1230/cs1230-final/house/untitled.mtl");
-    std::unordered_map<std::string, Material> materials = parseMTL("C:/Users/dhlee/OneDrive/Desktop/cs1230/cs1230-final/house/untitled.mtl");
+    std::unordered_map<std::string, Material> materials = parseMTL(mtlPath);
 
     std::vector<unsigned int> vertexIndices, uvIndices, normalIndices;
     std::vector<std::string> materialNames;
@@ -109,15 +116,18 @@ bool objparser::loadOBJ(const char * path, std::vector<float> & out_vertices){
 
     while(1){
         char lineHeader[256];
+
         int res = fscanf(file, "%s", lineHeader);
         if (res == EOF){
+            std::cout << "EOF " <<std::endl;
             break;
         }
 
-        if ( strcmp( lineHeader, "v" ) == 0 ){
+        if ( strcmp( lineHeader, "v" ) == 0){
             glm::vec3 vertex;
             fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z );
             temp_vertices.push_back(vertex);
+
 
         } else if ( strcmp( lineHeader, "vt" ) == 0 ){
             glm::vec2 uv;
@@ -219,6 +229,183 @@ bool objparser::loadOBJ(const char * path, std::vector<float> & out_vertices){
     }
     return true;
 }
+
+bool objparser::loadOBJBalloons(const char * path, std::vector<std::vector<float>> &out_vertices){
+
+    std::string mtlPath = path;
+    size_t dotPosition = mtlPath.find_last_of('.');
+    if (dotPosition != std::string::npos) {
+        mtlPath.replace(dotPosition, std::string::npos, ".mtl");
+    }
+
+    std::unordered_map<std::string, Material> materials = parseMTL(mtlPath);
+
+    std::vector<int> vertexIndices, uvIndices, normalIndices, balloonIndices;
+    std::vector<std::string> materialNames;
+    std::vector<glm::vec3> temp_vertices;
+    std::vector<glm::vec2> temp_uvs;
+    std::vector<glm::vec3> temp_normals;
+    std::string activeMaterial;
+
+    FILE * file = fopen(path, "r");
+    if( file == NULL ){
+        printf("Impossible to open the file !\n");
+        return false;
+    }
+    //std::map<std::string, BoundingBox> balloonBoundingBoxes;
+    int groupNum = 0;
+
+    while(1){
+        char lineHeader[256];
+
+        int res = fscanf(file, "%s", lineHeader);
+
+        if (res == EOF){
+            std::cout << "EOF Balloons" <<std::endl;
+            break;
+        }
+
+
+
+        if (strcmp(lineHeader, "o") == 0){
+            char objType[256];
+            fscanf(file, "%s", objType);
+            if (strncmp(objType, "Sphere", 6) == 0) {
+                groupNum++;
+            }
+            continue;
+        }
+
+
+        if ( strcmp( lineHeader, "v" ) == 0){
+            glm::vec3 vertex;
+            fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z );
+            temp_vertices.push_back(vertex);
+
+
+        } else if ( strcmp( lineHeader, "vt" ) == 0 ){
+
+            glm::vec2 uv;
+            fscanf(file, "%f %f\n", &uv.x, &uv.y);
+            temp_uvs.push_back(uv);
+
+
+        } else if (strcmp( lineHeader, "vn" ) == 0 ){
+
+            glm::vec3 normal;
+            fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+            temp_normals.push_back(normal);
+
+        } else if (strcmp( lineHeader, "f" ) == 0 ){
+
+            unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
+            int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n",
+                                 &vertexIndex[0], &uvIndex[0], &normalIndex[0],
+                                 &vertexIndex[1], &uvIndex[1], &normalIndex[1],
+                                 &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
+
+            if (matches != 9){
+                std::cout << "Expected 9 matches, but got: " << matches << std::endl;
+
+                printf("File can't be read by our simple parser : ( Try exporting with other options\n");
+                return false;
+            }
+            vertexIndices.push_back(vertexIndex[0]);
+            vertexIndices.push_back(vertexIndex[1]);
+            vertexIndices.push_back(vertexIndex[2]);
+
+            uvIndices.push_back(uvIndex[0]);
+            uvIndices.push_back(uvIndex[1]);
+            uvIndices.push_back(uvIndex[2]);
+
+            normalIndices.push_back(normalIndex[0]);
+            normalIndices.push_back(normalIndex[1]);
+            normalIndices.push_back(normalIndex[2]);
+
+            materialNames.push_back(activeMaterial);
+            materialNames.push_back(activeMaterial);
+            materialNames.push_back(activeMaterial);
+
+            balloonIndices.push_back(groupNum);
+            balloonIndices.push_back(groupNum);
+            balloonIndices.push_back(groupNum);
+        }
+        else if (strncmp(lineHeader, "usemtl", 6) == 0 ) {
+
+            char temp_material_name[256];
+            fscanf(file, "%255s\n", temp_material_name);
+            activeMaterial = temp_material_name;
+        }
+    }
+
+    int balloonNumCurrent = 0;
+    std::vector<float> balloonVerticesFinal;
+    // Now that we have the vertex, uv, normal indices, and materials, calculate tangents
+    for( unsigned int i = 0; i < vertexIndices.size(); i += 3 ){
+        // Get the three vertices and uvs for each triangle
+        glm::vec3 v1 = temp_vertices[vertexIndices[i] - 1];
+        glm::vec3 v2 = temp_vertices[vertexIndices[i+1] - 1];
+        glm::vec3 v3 = temp_vertices[vertexIndices[i+2] - 1];
+        glm::vec2 uv1 = temp_uvs[uvIndices[i] - 1];
+        glm::vec2 uv2 = temp_uvs[uvIndices[i+1] - 1];
+        glm::vec2 uv3 = temp_uvs[uvIndices[i+2] - 1];
+
+        int balloonNum = balloonIndices[i];
+        if (balloonNum != balloonNumCurrent) {
+            out_vertices.push_back(balloonVerticesFinal);
+            balloonVerticesFinal.clear();
+            balloonNumCurrent = balloonNum;
+        }
+
+        // Calculate the tangent for this triangle
+        glm::vec3 tangent = calculateTangent(v1, v2, v3, uv1, uv2, uv3);
+
+        // Now add the vertices, normals, UVs, material properties, and tangent
+        for (unsigned int j = 0; j < 3; j++) {
+            unsigned int vertexIndex = vertexIndices[i + j];
+            glm::vec3 vertex = temp_vertices[vertexIndex - 1];
+            balloonVerticesFinal.push_back(vertex.x);
+            balloonVerticesFinal.push_back(vertex.y);
+            balloonVerticesFinal.push_back(vertex.z);
+
+            unsigned int normalIndex = normalIndices[i + j];
+            glm::vec3 normal = temp_normals[normalIndex - 1];
+            balloonVerticesFinal.push_back(normal.x);
+            balloonVerticesFinal.push_back(normal.y);
+            balloonVerticesFinal.push_back(normal.z);
+
+            //material
+            Material mat = materials[materialNames[i + j]];
+            balloonVerticesFinal.push_back(mat.Ka[0]);
+            balloonVerticesFinal.push_back(mat.Ka[1]);
+            balloonVerticesFinal.push_back(mat.Ka[2]);
+
+            balloonVerticesFinal.push_back(mat.Kd[0]);
+            balloonVerticesFinal.push_back(mat.Kd[1]);
+            balloonVerticesFinal.push_back(mat.Kd[2]);
+
+            balloonVerticesFinal.push_back(mat.Ks[0]);
+            balloonVerticesFinal.push_back(mat.Ks[1]);
+            balloonVerticesFinal.push_back(mat.Ks[2]);
+
+            balloonVerticesFinal.push_back(mat.Ns);
+
+
+            // UV coordinates
+            unsigned int uvIndex = uvIndices[i + j];
+            glm::vec2 uv = temp_uvs[uvIndex - 1];
+            balloonVerticesFinal.push_back(uv.x);
+            balloonVerticesFinal.push_back(uv.y);
+
+            //tangent
+            balloonVerticesFinal.push_back(tangent.x);
+            balloonVerticesFinal.push_back(tangent.y);
+            balloonVerticesFinal.push_back(tangent.z);
+        }
+    }
+    return true;
+}
+
 
 
 //calculation of tangents for bump mapping
